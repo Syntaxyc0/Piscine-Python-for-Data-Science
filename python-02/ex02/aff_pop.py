58daab599ea40d52df85dd4840bb5f7c26da21c4
Syntaxyc0 <jeanbesnier@yahoo.fr>
2024-02-13 11:48:53 +0100

    aff_pop

--- python-02/ex02/aff_pop.py
+++ python-02/ex02/aff_pop.py
@@ -4,14 +4,19 @@ from load_csv import load
 
 
 def main():
-    table = load("life_expectancy_years.csv")
+    table = load("population_total.csv")
+    years = table.columns[0:252]
     table.set_index("country", inplace = True)
-    table = table.loc['France']
-    plt.plot(table)
+    france = table.loc['France'][0:252]
+    Pakistan = table.loc['Pakistan'][0:252]
+    plt.figure(figsize=(10,6))
+    plt.plot(years, france, label='France')
+    plt.plot(years, Pakistan, label='Pakistan')
     plt.xlabel('Year')
-    plt.xticks(["1800", "1840", "1880", "1920", "1960", "2000", "2040", "2080"], rotation=0)
-    plt.ylabel('Life expectancy')
-    plt.title("France Life expectancy Projections")
+    plt.xticks(["1800", "1840", "1880", "1920", "1960", "2000", "2040"], rotation=0)
+    plt.ylabel('Population')
+    plt.title("Population Projections")
+    plt.legend()
     plt.show()
 
 
